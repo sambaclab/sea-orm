89d7de18ca9d53a0d0b6e24667333a03bba0e9df
Billy Chan <ccw.billy.123@gmail.com>
2025-02-19 06:36:24 +0800

    Support PgVector (#2500)
    * change sea-query to git
    
    * add column type
    
    * change to fork
    
    * vector
    
    * add feature flag
    
    * trygetable for vector
    
    * fix eq for vector
    
    * add size to vector
    
    * fix: pgvector version
    
    * Update Cargo.toml
    
    * Support PgVector
    
    * Use `pgvector/pgvector` docker image
    
    * Apply suggestions from code review
    
    * Fixup
    
    * Update tests/embedding_tests.rs
    
    ---------
    
    Co-authored-by: Leon Camus <leon.c@gmx.de>
    Co-authored-by: Leon Camus <leon.camus@tu-darmstadt.de>
    Co-authored-by: Chris Tsang <chris.2y3@outlook.com>

--- sea-orm-codegen/src/entity/base_entity.rs
+++ sea-orm-codegen/src/entity/base_entity.rs
@@ -272,6 +272,7 @@ impl Entity {
             match col_type {
                 ColumnType::Float | ColumnType::Double => true,
                 ColumnType::Array(col_type) => is_floats(col_type),
+                ColumnType::Vector(_) => true,
                 _ => false,
             }
         }
